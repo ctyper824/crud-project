b9811705ee1a6bc34d2a65bee6d698e858f971bf
ctyper824 <ctyper824@gmail.com>
2025-03-05 08:59:18 -0800

    commit some letters :pencil:

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -104,7 +104,7 @@ const App = () => {
   return (
     <div>
       <div className="container">
-        <h1>My MERN Stack Project (CRUD)</h1>
+        <h1>Our MERN Stack Project (CRUD)</h1>
         <form onSubmit={handleAddUser}>
           <input
             type="text"
