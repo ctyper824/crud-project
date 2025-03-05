610f6cbd0344d54848519dfda7d00c2cea12c74d
ctyper824 <ctyper824@gmail.com>
2025-03-05 08:53:17 -0800

    fixed server base url :hammer:

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   const [userBtn, setUserBtn] = useState("Add user");
   const [userVariant, setUserVariant] = useState({ bool: true, id: "" });
   //  API URI's
-  const BASE_URL = "https://crud-project-backend.vercel.app/api";
+  const BASE_URL = "https://crud-project-server-sooty.vercel.app/api";
   const getUsers = `${BASE_URL}/users`;
   const postUser = `${BASE_URL}/addUser`;
 
