aa0795b19dc27bb8203d6a4dbbd52f08ee062f23
ctyper824 <ctyper824@gmail.com>
2025-03-05 09:27:47 -0800

    added env file :+1:

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
-// const mongoURI = 'mongodb+srv://mz7731346:kGdWOtV7Wto1kkxI@cluster0.r7mtc.mongodb.net/myfirstdatabase?retryWrites=true&w=majority&appName=Cluster0';
-const mongoURI = "mongodb+srv://newuser:PO8r8Yoo8PrQvhNA@cluster0.gzcwm.mongodb.net/testdb?retryWrites=true&w=majority&appName=Cluster0";
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const mongoURI = process.env.mongoURI;
 // const mongoURI = 'mongodb://localhost:27017/crud';
 
 const connectDB = async () => {
